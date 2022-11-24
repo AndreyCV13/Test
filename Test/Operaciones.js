8f6ad6539f48f26675951b7536d0d410d78827af
Andrey Calvo <andreycv4@gmail.com>
2022-11-24 14:05:55 -0600

    Agregando primer test

---
+++ Test/Operaciones.js
@@ -0,0 +1,24 @@
+var assert = require('assert');
+
+describe('Caja de operaciones básicas', () =>{
+    var a=10;
+    var b=10;
+    var results=0;
+    it('Suma de dos números', () =>{
+        results=a+b;
+        assert.equal(results,20);
+    })
+    it('Resta de dos números', () =>{
+        results=a-b;
+        assert.equal(results,0);
+    })
+    it('Multiplicación de dos números', () =>{
+        results=a*b;
+        assert.equal(results,100);
+    })
+    it('División de dos números', () =>{
+        results=a/b;
+        assert.equal(results,1);
+    })
+
+})
